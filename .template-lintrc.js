4298dae73f1a8669dab1d571f4c5b5eb75128e68
abeidahmed <abeidahmed92@gmail.com>
2021-07-14 22:25:27 +0530

    configure linter

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -2,4 +2,11 @@
 
 module.exports = {
   extends: 'recommended',
+  rules: {
+    'no-html-comments': false,
+    'no-implicit-this': { allow: ['value'] },
+    'img-alt-attributes': false,
+    'self-closing-void-elements': false,
+    quotes: false,
+  },
 };
