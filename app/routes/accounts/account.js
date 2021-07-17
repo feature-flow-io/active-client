f07c27392422412811e4da68e15569e61794874d
abeidahmed <abeidahmed92@gmail.com>
2021-07-17 15:19:07 +0530

    authenticate account URL

--- app/routes/accounts/account.js
+++ app/routes/accounts/account.js
@@ -3,6 +3,16 @@ import { inject as service } from '@ember/service';
 
 export default class AccountsAccountRoute extends Route {
   @service store;
+  @service session;
+  @service router;
+
+  async beforeModel(transition) {
+    super.beforeModel(transition);
+
+    if (!this.session.currentUserToken) {
+      this.router.transitionTo('login');
+    }
+  }
 
   async model({ account_id }) {
     return this.store.findRecord('account', account_id);
--- app/routes/accounts/new.js
+++ app/routes/accounts/new.js
@@ -1,3 +1,15 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class AccountsNewRoute extends Route {}
+export default class AccountsNewRoute extends Route {
+  @service session;
+  @service router;
+
+  async beforeModel(transition) {
+    super.beforeModel(transition);
+
+    if (!this.session.currentUserToken) {
+      this.router.transitionTo('login');
+    }
+  }
+}
