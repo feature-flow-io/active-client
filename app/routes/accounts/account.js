5bebd161a8995e6d70cc3045e5c56d4ad37e8c4c
abeidahmed <abeidahmed92@gmail.com>
2021-07-17 15:13:37 +0530

    render account info

--- app/routes/accounts/account.js
+++ app/routes/accounts/account.js
@@ -1,7 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class AccountsAccountRoute extends Route {
-  async model() {
-    return 'hello world';
+  @service store;
+
+  async model({ account_id }) {
+    return this.store.findRecord('account', account_id);
   }
 }
