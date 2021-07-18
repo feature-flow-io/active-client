a11a00bc3868dfecbb42b51eddf14ec02b3950b2
abeidahmed <abeidahmed92@gmail.com>
2021-07-18 20:03:12 +0530

    test rename account form

--- mirage/config.js
+++ mirage/config.js
@@ -1,9 +1,22 @@
 export default function () {
   this.namespace = 'v1';
 
+  this.post('/accounts', function (schema) {
+    const attrs = this.normalizedRequestAttrs();
+
+    return schema.accounts.create(attrs);
+  });
+
   this.get('/accounts/:id', function (schema, request) {
     const id = request.params.id;
 
     return schema.accounts.find(id);
+  });
+
+  this.patch('/accounts/:id', function (schema, request) {
+    const id = request.params.id;
+    const attrs = this.normalizedRequestAttrs();
+
+    return schema.accounts.find(id).update(attrs);
   });
 }
---
+++ tests/acceptance/general-setting-test.js
@@ -0,0 +1,30 @@
+import { module, test } from 'qunit';
+import { visit, currentURL, fillIn, click, find } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+import MockSessionService from '../stubs/mock-session';
+
+module('Acceptance | general setting', function (hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  hooks.beforeEach(function () {
+    this.owner.register('service:session', MockSessionService);
+    const user = this.server.create('user');
+    this.owner.lookup('service:session').currentUserToken = user.token;
+  });
+
+  test('renaming the account', async function (assert) {
+    const account = this.server.create('account');
+    await visit(`/accounts/${account.id}/settings`);
+
+    assert.equal(currentURL(), `/accounts/${account.id}/settings`);
+
+    assert.equal(find('#account-name').value, account.name);
+    assert.true(find('button[data-test-id="rename-button"]').disabled);
+
+    await fillIn('#account-name', 'My new updated account');
+    await click('button[data-test-id="rename-button"]');
+    assert.equal(this.server.db.accounts[0].name, 'My new updated account');
+  });
+});
