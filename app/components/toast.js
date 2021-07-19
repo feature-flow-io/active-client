580349d1526d97381877192ad70b147c6139d7f3
abeidahmed <abeidahmed92@gmail.com>
2021-07-19 19:07:40 +0530

    improve toast component

--- app/components/toast.js
+++ app/components/toast.js
@@ -2,6 +2,9 @@ import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 
+const SERVICE_DELAY = 300;
+const DELAY = 4000;
+
 export default class ToastComponent extends Component {
   @service notification;
 
@@ -9,26 +12,31 @@ export default class ToastComponent extends Component {
     event.preventDefault();
     const toast = document.getElementById(id);
     toast.classList.replace('toast--animateIn', 'toast--animateOut');
-    this.fadeoutId = null;
+    this.abortFadeout();
 
-    this.timeoutId = setTimeout(() => {
-      this.notification.removeNotification(id);
-    }, 300);
+    setTimeout(() => this.removeNotification(id), SERVICE_DELAY);
   }
 
   @action fadeOut(element) {
-    this.fadeoutId = setTimeout(() => {
+    this.timeoutId = setTimeout(() => {
       element.classList.replace('toast--animateIn', 'toast--animateOut');
 
-      setTimeout(() => {
-        this.notification.removeNotification(element.id);
-      }, 300);
-    }, 4000);
+      setTimeout(() => this.removeNotification(element.id), SERVICE_DELAY);
+    }, DELAY);
   }
 
   willDestroy() {
     super.willDestroy(...arguments);
+    this.abortFadeout();
+  }
+
+  // private
+
+  removeNotification(id) {
+    this.notification.removeNotification(id);
+  }
+
+  abortFadeout() {
     if (this.timeoutId) clearTimeout(this.timeoutId);
-    if (this.fadeoutId) clearTimeout(this.fadeoutId);
   }
 }
