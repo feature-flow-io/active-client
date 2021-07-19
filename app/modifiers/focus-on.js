5188371832a53e3f5847d6e25e430269b136dc4a
abeidahmed <abeidahmed92@gmail.com>
2021-07-19 11:31:28 +0530

    add deactivate modal

---
+++ app/modifiers/focus-on.js
@@ -0,0 +1,11 @@
+import { modifier } from 'ember-modifier';
+import { nextFrame } from '../utils/timing-helpers';
+
+export default modifier(function focusOn(element, [eventName, targetElement]) {
+  async function handleFocus() {
+    await nextFrame();
+    document.getElementById(targetElement).focus();
+  }
+
+  element.addEventListener(eventName, handleFocus);
+});
